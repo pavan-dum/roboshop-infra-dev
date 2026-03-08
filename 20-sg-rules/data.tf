5f71e2434cec5d7b4676521bab9967c0fdd17e69
pavan-dum <pavankumardumpala3941@gmail.com>
2026-03-08 23:47:25 +0530

    actions

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -14,7 +14,9 @@ data "aws_ssm_parameter" "mongodb_sg_id" {
     name = "/${var.project}/${var.Environment}/mongodb_sg_id"
 }
 
-
+data "aws_ssm_parameter" "redis_sg_id" {
+    name = "/${var.project}/${var.Environment}/redis_sg_id"
+}
 
 data "aws_ssm_parameter" "catalogue_sg_id" {
     name = "/${var.project}/${var.Environment}/catalogue_sg_id"

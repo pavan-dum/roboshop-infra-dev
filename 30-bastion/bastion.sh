fd73cc270df450006f84aefa6bc349d3bff57c07
pavan-dum <pavankumardumpala3941@gmail.com>
2026-03-08 22:45:22 +0530

    actions

--- 30-bastion/bastion.sh
+++ 30-bastion/bastion.sh
@@ -2,10 +2,13 @@
 
 # We are creating 50GB root disk, but only 20GB is partitioned
 # Remaining 30GB we need to extend using below commands
+
+
 growpart /dev/nvme0n1 4
 pvresize /dev/nvme0n1p4
 
-lvextend -r -l +100%FREE /dev/mapper/RootVG-rootVol
+lvextend -r -L +30G /dev/mapper/RootVG-homeVol
+xfs_growfs /home
 
 yum install -y yum-utils
 yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo

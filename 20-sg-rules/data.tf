58c0ef0185b59e8e52602fcf012e0868a7a574ee
pavan-dum <pavankumardumpala3941@gmail.com>
2026-03-11 08:29:19 +0530

    actions

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -33,3 +33,7 @@ data "aws_ssm_parameter" "catalogue_sg_id" {
 data "aws_ssm_parameter" "user_sg_id" {
     name = "/${var.project}/${var.Environment}/user_sg_id"
 }
+
+data "aws_ssm_parameter" "backend_alb_sg_id" {
+    name = "/${var.project}/${var.Environment}/backend_alb_sg_id"
+}

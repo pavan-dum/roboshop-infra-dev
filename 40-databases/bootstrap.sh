360566029f0cf7c05dde774e3c4e29553549f529
pavan-dum <pavankumardumpala3941@gmail.com>
2026-03-11 08:39:36 +0530

    actions

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -9,4 +9,4 @@ git clone https://github.com/pavan-dum/ansible-roboshop-roles-tf.git
 
 cd ansible-roboshop-roles-tf
 git pull
-ansible-playbook -e component=$component -e environment=$Environment roboshop.yaml
+ansible-playbook -e component=$component -e env=$Environment roboshop.yaml

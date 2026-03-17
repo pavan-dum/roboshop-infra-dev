40672a88981ef84253aa679ed6bdb6466390f814
pavan-dum <pavankumardumpala3941@gmail.com>
2026-03-17 08:54:07 +0530

    actions

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "frontend_alb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [local.frontend_alb_sg_id]
-  subnets            = local.private_subnet_ids
+  subnets            = local.public_subnet_ids
 
   enable_deletion_protection = false
 

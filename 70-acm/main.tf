c9996b3dbbd14c9247c154c776c0001f6034cfa3
pavan-dum <pavankumardumpala3941@gmail.com>
2026-03-17 08:07:16 +0530

    actions

--- 70-acm/main.tf
+++ 70-acm/main.tf
@@ -31,7 +31,7 @@ resource "aws_route53_record" "roboshop" {
   records         = [each.value.record]
   ttl             = 60
   type            = each.value.type
-  zone_id         = var.zone_id
+  zone_id         = var.Zone_id
 }
 
 

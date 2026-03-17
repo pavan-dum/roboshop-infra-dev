b02ebce31874fdbe1a227758135653c25071fe1e
pavan-dum <pavankumardumpala3941@gmail.com>
2026-03-17 08:56:52 +0530

    actions

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -18,7 +18,7 @@ resource "aws_lb" "frontend_alb" {
 
 
 
-resource "aws_lb_listener" "http" {
+resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.frontend_alb.arn
   port              = "443"
   protocol          = "HTTPS"

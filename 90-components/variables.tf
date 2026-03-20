15b8d836b1fb6faf2f2c2c92181f1a866d0400c0
pavan-dum <pavankumardumpala3941@gmail.com>
2026-03-20 17:19:03 +0530

    actions

--- 90-components/variables.tf
+++ 90-components/variables.tf
@@ -22,21 +22,5 @@ variable "components" {
 }
 
 
-variable "project" {
-    default = "roboshop"
-}
-
-variable "Environment" {
-    default = "dev"
-}
-
-
-variable "Zone_id" {
-    default = "Z02160638EY77GSSE3BP"
-}
-
-variable "domain_name" {
-    default = "dpavan.online"
-}
 
 

82a0dce9ea4f415d1d70664b71ab6dfbf423c6cd
pavan-dum <pavankumardumpala3941@gmail.com>
2026-03-20 18:45:04 +0530

    actions

---
+++ 90-components/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+component=$1
+Environment=$2
+dnf install ansible -y
+
+cd /home/ec2-user
+git clone https://github.com/pavan-dum/ansible-roboshop-roles-tf.git
+
+cd ansible-roboshop-roles-tf
+git pull
+ansible-playbook -e component=$component -e env=$Environment roboshop.yaml
